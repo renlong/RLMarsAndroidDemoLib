35f5686f228f3e1a3402781a8064051fe247b6b7
renlong <renlong@taptap.com>
2022-04-17 18:28:31 +0800

    feat: 创建DemoLib项目

---
+++ demo-lib/src/main/java/com/taptap/demolib/DemoClass.kt
@@ -0,0 +1,14 @@
+package com.taptap.demolib
+
+import android.util.Log
+
+/**
+ * Created by renlong on 2022/4/17.
+ */
+
+class DemoClass {
+
+    fun print() {
+        Log.e("DemoClass", "111111")
+    }
+}
